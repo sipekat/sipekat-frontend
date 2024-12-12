70596da4b70f1a45e79e3e14c748d3fd612a3eb4
anandazio21 <anandafabrizio21@gmail.com>
2024-12-12 21:33:18 +0700

    Update laporanPage.js
    Memperbarui kode pada halaman Laporan Saya

--- src/pages/masyarakat/laporanPage.js
+++ src/pages/masyarakat/laporanPage.js
@@ -1,8 +1,31 @@
 const DashboardPage = {
   async render() {
     return `
-			<h1>Laporan saya</h1>
-		  `;
+		<section class="report-section">
+            <h2>Laporan Terkini <span class="status">status</span></h2>
+            <div class="report-list">
+                <!-- Laporan 1 -->
+                <div class="report-item">
+                    <h3>Jalan Berlubang</h3>
+                    <p class="location">Lokasi: JL.Kota NO. 123</p>
+                    <span class="status-label pending">Menunggu Verifikasi</span>
+                </div>
+                <!-- Laporan 2 -->
+                <div class="report-item">
+                    <h3>Lampu Jalan Mati</h3>
+                    <p class="location">Lokasi: Taman Kota</p>
+                    <span class="status-label processing">Dalam Proses</span>
+                </div>
+                <!-- Laporan 3 -->
+                <div class="report-item">
+                    <h3>Sampah Menumpuk</h3>
+                    <p class="location">Lokasi: Pasar Sentral</p>
+                    <span class="status-label completed">Selesai</span>
+                </div>
+            </div>
+        </section>
+		
+	  `;
   },
 
   async afterRender() {},
